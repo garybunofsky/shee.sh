4c70031df5adbc7fff2d60f86d9a1b81736545bc
garybunofsky <garybunofsky@gmail.com>
2016-10-07 10:05:11 -0400

    Removed unnecessary extra space.

--- shee.sh
+++ shee.sh
@@ -3,7 +3,7 @@
 # Copyright (c) 2016 Gary Bunofsky
 
 while true; do
-    read -p $'\e[92mShee.sh initialized\e[0m\n Open work applications? Please answer \e[92myes\e[0m or \e[91mno\e[0m. ' yn
+    read -p $'\e[92mShee.sh initialized\e[0m\nOpen work applications? Please answer \e[92myes\e[0m or \e[91mno\e[0m. ' yn
     case $yn in
         [Yy]* ) echo "Thanks, opening your work applications..."; open -a atext; open -a 'google chrome'; open -a atom; open -a 'mamp'; open -a 'nylas n1'; open -a 'slack'; open -a 'spectacle'; open -a 'sequel pro.app'; open -a 'spotify'; exit;;
         [Nn]* ) echo "Thanks, opening your play applications"; open -a 'nylas n1'; open -a 'atext'; open -a 'spectacle'; open -a 'spotify';  exit;;
